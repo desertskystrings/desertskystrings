fa241b87d9136b3d062eedf1511a322e44c7c344
DustNBows <bowanddust@gmail.com>
2026-09-18 04:12:46 +0000

    import css in entrypoint js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-// import * as css from "./css/style.css";
+import "./css/bootstrap-icons.css";
+import "./css/bootstrap.min.css";
+
+import * as css from "./css/style.css";
+
 // import * as cssmap from "./css/style.css.map";
 
